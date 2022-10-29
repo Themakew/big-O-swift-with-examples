9fb67ade861b82e6bf4de312160dd2c50fcfb3e2
Ruyther <ruy_fusion@hotmail.com>
2022-10-29 16:29:58 -0300

    Fixing example of O(n) Big-O

--- O(n)/BigOSwiftExample.playground/Contents.swift
+++ O(n)/BigOSwiftExample.playground/Contents.swift
@@ -16,34 +16,111 @@ import UIKit
 
  */
 
-func solution(A: [Int], K: Int) -> [Int] {
-    // Edge case
-    guard !A.isEmpty else {
-        return []
+
+/// Definition of Node
+final class ListNode {
+    var val: Int
+    var next: ListNode?
+
+    init() {
+        self.val = 0
+        self.next = nil
     }
 
-    // Edge case
-    guard A.count != 1 || K > 0 else {
-        return A
+    init(_ val: Int) {
+        self.val = val
+        self.next = nil
     }
 
-    var resultList = A
-
-    for _ in 1...K { // O(n) - for loop
-        if let lastItem = resultList.last {
-            resultList.removeLast() // O(1)
-            resultList.insert(lastItem, at: 0) // O(i) - where 'i' is the index, in this case, i = 0. Then, O(1) always.
-        }
+    public init(_ val: Int, _ next: ListNode?) {
+        self.val = val
+        self.next = next
     }
-
-    return resultList
 }
 
-solution(A: [1, 2, 3, 4, 5], K: 1) // 5 1 2 3 4
-solution(A: [1, 2, 3, 4, 5], K: 2) // 4 5 1 2 3
-solution(A: [1, 2, 3, 4, 5], K: 3) // 3 4 5 1 2
+extension ListNode: CustomStringConvertible {
+    var description: String {
+        guard let next = next else {
+            return "\(val)"
+        }
+        return "\(val) " + String(describing: next) + " "
+    }
+}
 
-solution(A: [3, 8, 9, 7, 6], K: 3) // [9, 7, 6, 3, 8]
+class Solution {
+    func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
+        // Edge case
+        if head?.next == nil {
+            return head
+        }
+
+        // Edge case
+        if head == nil {
+            return nil
+        }
+
+        // Distance of the current head to the future stack head
+        let offset = k % count(head)
+
+        // Edge case
+        guard offset > 0 else {
+            return head
+        }
+
+        var fast = head
+        var slow = head
+
+        // Setting fast pointer in the future stack head
+        for _ in 1...offset { // O(n) - for loop
+            fast = fast?.next
+        }
+
+        // Looping to update fast and slow pointers
+        while fast?.next != nil { // O(n) - while loop
+            fast = fast?.next
+            slow = slow?.next
+        }
+
+        // Setting new head
+        let newHead = slow?.next
+        slow?.next = nil
+        fast?.next = head
+
+        return newHead
+    }
+
+    private func count(_ head: ListNode?) -> Int {
+        var currentNode = head
+        var result = 0
+
+        while currentNode != nil {
+            currentNode = currentNode?.next
+            result+=1
+        }
+
+        return result
+    }
+}
+
+let solution = Solution()
+var node1: ListNode?
+
+func buildStack() {
+    let node5 = ListNode(5)
+    let node4 = ListNode(4, node5)
+    let node3 = ListNode(3, node4)
+    let node2 = ListNode(2, node3)
+    node1 = ListNode(1, node2)
+}
+
+buildStack()
+solution.rotateRight(node1, 1) // 5 1 2 3 4
+
+buildStack()
+solution.rotateRight(node1, 2) // 4 5 1 2 3
+
+buildStack()
+solution.rotateRight(node1, 3) // 3 4 5 1 2
 
 // TIME COMPLEXITY = O(n)
 // SPACE COMPLEXITY = O(n)
